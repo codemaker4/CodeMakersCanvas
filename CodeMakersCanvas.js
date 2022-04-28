8a000f3dff94c8e80e030af1ede7d694c814ec6f
codemaker4 <thijs.rutledge@coderclass.nl>
2022-04-28 14:34:11 +0200

    made function for checking imports

--- CodeMakersCanvas.js
+++ CodeMakersCanvas.js
@@ -11,13 +11,8 @@ class CmCanvas {
      * @param {string} canvasId The id of the canvas you want to use.
      */
     constructor(canvasId) {
-        try {
-            if (!Vec2d) throw "CodeMakersCanvas requires github.com/codemaker4/vec2d";
-        } catch (e) {
-            throw "CodeMakersCanvas requires github.com/codemaker4/vec2d";
-        }
-        if (Vec2d.library !== "codemaker4/vec2d") console.warn("You seem to be using an alternative Vec2d library. This can work fine, but it is better to use github.com/codemaker4/vec2d instead.");
-        if (Vec2d.library == "codemaker4/vec2d" && Vec2d.version < 1) console.warn("You seem to be using an older version of github.com/codemaker4/vec2d. Please directly use the github link to always use the latest version.");
+        CmCanvas.chekLib(Vec2d, "codemaker4/vec2d", 1); // check for github.com/codemaker4/vec2d
+
         /**
          * The selected canvas element.
          * @type {HTMLCanvasElement}
@@ -54,6 +49,17 @@ class CmCanvas {
          * The X-position of the mouse
          */
         this.mouseX = 0;
+    }
+    /**
+     * Check if a given library exists and is
+     * @param {function} lib The class/constructor of the library
+     * @param {string} libName The unique github link of the library, like "codemaker4/vec2d"
+     * @param {number} minVersion The minimum version required
+     */
+    static chekLib(lib, libName, minVersion) {
+        if (!lib) throw `CodeMakersCanvas requires github.com/${libName}`;
+        if (lib.library !== libName) console.warn(`You seem to be using an alternative ${libName} library. This can work fine, but it is better to use github.com/${libName} instead.`);
+        if (lib.library == libName && lib.version < minVersion) console.warn(`You seem to be using an older version of github.com/${libName}. Please directly use the latest version.`);
     }
     /**
      * Clear the canvas.
