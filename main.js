714f62f4f83f3469a17369258b4380c78fd2cbf9
codemaker4 <thijs.rutledge@coderclass.nl>
2022-04-28 14:40:54 +0200

    added test for CmCanvas.mouseX and .mouseY

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ function draw() {
     cmCanvas.clear();
 
     ctx.fillStyle = "#f00";
-    ctx.fillRect((Math.cos(counter/100)+1) * cmCanvas.width/3, (Math.sin(counter/100)+1) * cmCanvas.height/3, 10, 10);
+    // ctx.fillRect((Math.cos(counter/100)+1) * cmCanvas.width/3, (Math.sin(counter/100)+1) * cmCanvas.height/3, 10, 10);
+    ctx.fillRect(cmCanvas.mouseX, cmCanvas.mouseY, 10, 10);
 
     counter++;
 
