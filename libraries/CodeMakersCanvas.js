93c3c1fedd6aa9a7a91defde363b5cb1d8876319
codemaker4 <thijs.rutledge@coderclass.nl>
2022-05-20 11:58:44 +0200

    now using modules

--- libraries/CodeMakersCanvas.js
+++ libraries/CodeMakersCanvas.js
@@ -1,9 +1,10 @@
+import { Vec2d } from "./vec2d.js";
 /**
  * A class wrapping a html canvas with 2d rendering context.
  * @author CodeMaker_4
  * @version Beta_0.001
  */
-class CmCanvas {
+export class CmCanvas {
     static library = "codemaker4/CmCanvas";
     static verson = 0.001;
     /**
@@ -11,8 +12,6 @@ class CmCanvas {
      * @param {string} canvasId The id of the canvas you want to use.
      */
     constructor(canvasId) {
-        CmCanvas.chekLib(Vec2d, "codemaker4/vec2d", 1); // check for github.com/codemaker4/vec2d
-
         /**
          * The selected canvas element.
          * @type {HTMLCanvasElement}
@@ -118,21 +117,10 @@ class CmCanvas {
         }
     }
     /**
-     * Check if a given library exists and is
-     * @param {function} lib The class/constructor of the library
-     * @param {string} libName The unique github link of the library, like "codemaker4/vec2d"
-     * @param {number} minVersion The minimum version required
-     */
-    static chekLib(lib, libName, minVersion) {
-        if (!lib) throw `CodeMakersCanvas requires github.com/${libName}`;
-        if (lib.library !== libName) console.warn(`You seem to be using an alternative ${libName} library. This can work fine, but it is better to use github.com/${libName} instead.`);
-        if (lib.library == libName && lib.version < minVersion) console.warn(`You seem to be using an older version of github.com/${libName}. Please directly use the latest version.`);
-    }
-    /**
      * Clear the canvas.
      */
     clear() {
-        ctx.resetTransform()
-        ctx.clearRect(0,0,this.size.x, this.size.y);
+        this.ctx.resetTransform()
+        this.ctx.clearRect(0,0,this.size.x, this.size.y);
     }
 }
--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import { Vec2d } from "./libraries/vec2d.js";
+import { CmCanvas } from "./libraries/CodeMakersCanvas.js";
+
 let cmCanvas = new CmCanvas("mainCanvas");
 let ctx = cmCanvas.ctx;
 
@@ -15,14 +18,13 @@ function draw() {
     ctx.fillStyle = "#0f0";
     ctx.fillRect((Math.cos(counter/50)+1) * cmCanvas.size.x/2 - 10, (Math.sin(counter/50)+1) * cmCanvas.size.y/2 - 10, 20, 20);
 
-
     ctx.fillStyle = "#00f";
     ctx.fillRect(0,0,100,100);
     ctx.fillRect(cmCanvas.size.x-100,cmCanvas.size.y-100,100,100);
     ctx.fillRect(0,cmCanvas.size.y-100,100,100);
     ctx.fillRect(cmCanvas.size.x-100,0,100,100);
-
-    counter++;
+    
+    counter ++;
 
     requestAnimationFrame(draw);
 }
